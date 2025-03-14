a76afd7d9b1f35a84ceeeb06ae607902e605f08e
Kamlesh8584 <luciferlonewolf9@gmail.com>
2025-03-14 18:39:35 +0530

    Add files via upload

---
+++ tool kit/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    style.css
+</style>
+
+<body>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>3D Vertical Sections</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="section left">
+                <h2>TO DO LIST</h2>
+                <p> Tool to manage tasks efficiently.</p>
+                <a href="todo.html" target="_blank"> <BUTton><b>LIVE DEMO</b></BUTton> </a>
+            </div>
+            <div class="section middle">
+                <h2>AGE CALCULATOR</h2>
+                <p>The age calculator tool.  
+                    <br>
+                </p>
+                <a href="age.html" target="_blank"> <BUTton><b>LIVE DEMO</b></BUTton> </a>
+            </div>
+            <div class="section right">
+                <h2>CLICK COUNT</h2>
+                <p>This tool counts your clicks.
+
+
+                </p>
+                <a href="click.html" target="_blank"> <BUTton><b>LIVE DEMO</b></BUTton> </a>
+            </div>
+        </div>
+    </body>
+
+    </html>
+
+
+</body>
+
+</html>
